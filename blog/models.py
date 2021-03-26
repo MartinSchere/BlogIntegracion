cbdaa5c016babda17041754fce2ca737cbb9eb94
Guerra-Defranchi <guerra.defranchi@protonmail.com>
2021-03-26 19:40:24 -0300

    Se corrige ubicación de la clase User desde el models.py de la app blog a user

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,2 @@
 from django.db import models
 
-class User(models.Model):
-    username = models.CharField(max_length=100, unique=True)
-    full_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    password_hash = models.CharField(max_length=255)
-    password_confirm = models.CharField(max_length=255)
-    bio = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)

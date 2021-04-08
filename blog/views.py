8e502419a37222e11e1454404e323b982476395a
Jorge Espinoza <jorgeespinozaortega@gmail.com>
2021-04-08 19:46:23 -0400

    Se agrega lista de Posts y registramos modelos en admin

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post, Comment, Clap, Vote
 
-def index(request):
-    return render(request, "blog/index.html")
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/index.html"
+    context_object_name = "posts"

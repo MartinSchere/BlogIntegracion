0ca04d787cde672d333968dc672c9fc95e9c9185
Jorge Espinoza <jorgeespinozaortega@gmail.com>
2021-04-17 01:16:38 -0400

    Se puede hacer un nuevo post

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='index')
+    path('', views.PostListView.as_view(), name='index'),
+    path('new_post', views.PostCreateView.as_view(), name='new_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Post, Comment, Clap, Vote
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class PostListView(ListView):
     model = Post
     template_name = "blog/index.html"
     context_object_name = "posts"
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    fields = ['title', 'content']
+    model = Post
+    template_name = "blog/new_post.html"
+    success_url = "/"
+
+    def form_valid(self, form):
+        print(form)
+        post = form.save(commit=False)
+        post.user = self.request.user
+        post.save()
+        return super().form_valid(form)
+    
